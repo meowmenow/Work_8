a1e8b3a798f3d60610e2c77a05845b0d437f0f46
dartg <dartg@DESKTOP-32CQM3V>
2024-08-01 18:12:40 +0300

    Добавьте файлы проекта.

---
+++ Адовые коды №14 (Практическая намбер фортин) v2.0/Адовые коды №14 (Практическая намбер фортин) v2.0/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Бирюков Георгий, ИСП-23, 10.02.2022г.
+//Описать процедуру RectPS(x1, y1, x2, y2, P, S), вычисляющую периметр P и площадь S прямоугольника со сторонами, параллельными осям координат, по координатам (x1, y1), (x2, y2) 
+//Его противоположных вершин (x1, y1, x2, y2 — входные, P и S — выходные параметры вещественного типа).
+//С помощью этой процедуры найти периметры и площади трех прямоугольников с данными противоположными вершинами.
+
+namespace Адовые_коды__14__Практическая_намбер_фортин__v2._0
+{
+    class Program
+    {
+        static void RectPS(double x1, double x2, double y1, double y2)
+        {
+            double p, s;
+            Console.WriteLine("Полученные значения P, S: ");
+            p = (Math.Abs(x2 - x1) + Math.Abs(y2 - y1)) * 2; //Вычисление периметра
+            s = Math.Abs(x2 - x1) * Math.Abs(y2 - y1); //Вычисление площади           
+            Console.WriteLine(p);
+            Console.WriteLine(s);
+        }
+
+        static void Main(string[] args)
+        {
+            double x1, x2, y1, y2; //Ввод значений
+            Console.Write("Введите значение x1: ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите значение x2: ");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите значение y1: ");
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите значение y2: ");
+            y2 = Convert.ToInt32(Console.ReadLine());
+            RectPS(x1, x2, y1, y2); //Обращение к функции
+            Console.ReadKey(); //Пауза
+        }
+
+        
+
+
+
+
+    }
+}   
---
+++ Адовые коды №14 (Практическая работа намбер фортин)/Адовые коды №14 (Практическая работа намбер фортин)/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Практическая работа №14. ИСП-23, Биркюов Георгий, 27.01.2022
+//Описать функцию SumRange(A, B) целого типа, находящую сумму всех целых чисел от A до B включительно (A и B — целые). 
+//Если A > B, то функция возвращает 0. С помощью этой функции найти суммы чисел от A до B и от B до C, если даны числа A, B, C. 
+
+namespace Адовые_коды__14__Практическая_работа_намбер_фортин_
+{
+    class Program
+    {
+        static int SumRange(int A, int B) //Нахождение суммы A, B
+        {
+            int sum = 0;
+            if (A > B) //Если А больше B, то ретарн 0
+                return 0; //Возвращение нуля
+            else
+            {
+                for (int i = A; i <= B; i++)
+                    sum += i;
+                return sum; //Возврат ответа
+            }
+        }
+        static int SumRange(int A, int B, int C) //Нахождение суммы от A до B и от B до C
+        {
+            int sum = 0; //Сумма чисел
+
+            if (A > B || B > C) //от A до B и от B до С 
+                return 0; //Возвращение нуля
+            else
+            {
+                for (int i = A; i <= B; i++)
+                    sum += i;
+                for (int i = B; i <= C; i++)
+                    sum += i;
+                return sum;
+            }
+        }
+        static void Main(string[] args) //Главная программа
+        {
+           
+            Console.Write("Введите значение A: ");
+            int A = int.Parse(Console.ReadLine());
+            Console.Write("Введите значение B: ");
+            int B = int.Parse(Console.ReadLine());
+            Console.Write("Введите значение C: ");
+            int C = int.Parse(Console.ReadLine());
+            Console.WriteLine(SumRange(A, B)); //Вызов функции
+            Console.WriteLine(SumRange(A, B, C)); //Вызов функции
+            Console.ReadKey(); //
+            Console.ReadKey(); //Пауза
+
+        }
+    }
+}
